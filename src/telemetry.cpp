f87e3534ffae5f19628d883f2d57f62fe00a3fac
ok2ufo <144491494+ok2ufo@users.noreply.github.com>
2023-09-08 23:22:15 +0200

    Update telemetry.cpp

--- src/telemetry.cpp
+++ src/telemetry.cpp
@@ -6,27 +6,38 @@ extern Configuration  Config;
 void setup() {
   if (Config.telemetry.active) {
     bool status;
-    status = bme.begin(0x77);  // Don't forget to join pins for righ direction on BME280!
+    status = true;
     if (status) {
-      Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH:T#144,163,078,013,080,,1111,Solar Power iGate";
-    //Serial.println(telemetryPacket);
-    //espClient.write((telemetryPacket + "\n").c_str());
-    //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :PARM.Battery";
-    //Serial.println(telemetryPacket);
-    //espClient.write((telemetryPacket + "\n").c_str());
-    //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :UNIT.Vdc";
-    //Serial.println(telemetryPacket);
-    //espClient.write((telemetryPacket + "\n").c_str());
-    //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :EQNS.0,0.01,2.5";
-    //Serial.println(telemetryPacket);
-    //espClient.write((telemetryPacket + "\n").c_str());
-    //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :BITS.11111111,Solar Power iGate";
-    //Serial.println(telemetryPacket);
-    //espClient.write((telemetryPacket + "\n").c_str());
+      Serial.println("Sending Telemetry init packet!");
 
+      // 4x packet for setings aprs.fi for telemetery - now only Voltage Li-POL 2,5-4,2V
+      // INIT
+      telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH:T#100,100,100,100,100,,1111,Solar Power iGate";
+      Serial.println(telemetryPacket);
+      espClient.write((telemetryPacket + "\n").c_str());
+      sleep(1000);
+      // PARM  
+      telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :PARM.Battery";
+      Serial.println(telemetryPacket);
+      espClient.write((telemetryPacket + "\n").c_str());
+      sleep(1000);
+      // UNITS
+      telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :UNIT.Vdc";
+      Serial.println(telemetryPacket);
+      espClient.write((telemetryPacket + "\n").c_str());
+      sleep(1000);
+      //EQNS
+      telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :EQNS.0,0.01,2.5";
+      Serial.println(telemetryPacket);
+      espClient.write((telemetryPacket + "\n").c_str());
+      sleep(1000);
+      //BITS
+      telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :BITS.11111111,Solar Power iGate";
+      Serial.println(telemetryPacket);
+      espClient.write((telemetryPacket + "\n").c_str());
+      sleep(1000);
     } else {
-      Serial.println("init : BME280 Module  ...     done!");
+      Serial.println("init : Telemetry  ...     failed!");
     }
   } else {
     Serial.println("(TELEMETRY not 'active' in 'igate_conf.json')");
@@ -37,17 +48,10 @@ void setup() {
 void updateTelemetry() {
     String telemetryPacket;
 
-      //beaconPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH::OK2UFO-1 :BITS.11111111,Solar Power iGate";
-      //telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH:T#" + String(lastTelemetryPacket) + ",";
-
-   // UPDATE
-                lastTelemetryPacket++;
-                //telemetryPacket = iGateBeaconPacket;
-                telemetryPacket = "OK2UFO-1>APRS,TCPIP*,qAC,T2CZECH:T#" + String(lastTelemetryPacket) + ",";
-                telemetryPacket += String(float((BATTERY_Utils::checkVoltages()-2.5)/0.01),0);
-                telemetryPacket += ",0,0,0,0,00000000";
-                Serial.println(telemetryPacket);
-                espClient.write((telemetryPacket + "\n").c_str());
-                // ---------------------------------------------------------
-
-  }
+    lastTelemetryPacket++;
+    telemetryPacket = Config.callsign + ">APRS,TCPIP*,qAC,T2CZECH:T#" + String(lastTelemetryPacket) + ",";
+    telemetryPacket += String(float((BATTERY_Utils::checkVoltages()-2.5)/0.01),0);
+    telemetryPacket += ",0,0,0,0,00000000";
+    Serial.println(telemetryPacket);
+    espClient.write((telemetryPacket + "\n").c_str());
+}
